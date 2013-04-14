17adcff24263902e2630b04e6a535b28fcb0d948
Sam Gruber <grubermensch@gmail.com>
2013-04-14 13:25:02 -0400

    added generic display method

--- src/com/scgruber/graphlambda/Node.java
+++ src/com/scgruber/graphlambda/Node.java
@@ -8,4 +8,11 @@ public class Node {
 	public Node() {
 		this.pos = new PVector(0,0);
 	}
+	
+	public void display(PGraphics buf) {
+		buf.fill(0);
+		buf.stroke(0);
+		
+		buf.ellipse(pos.x, pos.y, 5.0f, 5.0f);
+	}
 }
