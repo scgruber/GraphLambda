c8bab85c6fd229342e51aa8495c98553952b3ffb
Sam Gruber <grubermensch@gmail.com>
2013-04-15 00:18:13 -0400

    dynamically fix sizes

--- src/com/scgruber/graphlambda/Group.java
+++ src/com/scgruber/graphlambda/Group.java
@@ -16,12 +16,12 @@ public class Group {
 	public Group(Group parent, Node output) {
 		this.parent = parent;
 		this.pos = new PVector(0,0);
-		this.radius = 10.0f;
+		this.radius = 25.0f;
 		this.inputs = new ArrayList();
 		this.interior = new ArrayList();
 		this.groups = new ArrayList();
 		this.output = new Output(output);
-		this.isDirty = false;
+		this.isDirty = true;
 	}
 	
 	/* Public methods */
@@ -30,12 +30,41 @@ public class Group {
 		if (isDirty) {
 			/* Update all properties that could have changed */
 			radius = calculateBestRadius();
+			distributeInputs();
+			output.setPos(new PVector(pos.x, pos.y+radius));
+			for (int i = interior.size()-1; i >= 0; i--) {
+				if (PVector.dist(pos, interior.get(i).getPos()) > radius) {
+					interior.get(i).setPos(pos.get());
+				}
+			}
+			for (int i = groups.size()-1; i >= 0; i--) {
+				if (PVector.dist(pos, groups.get(i).getPos()) > radius) {
+					groups.get(i).setPos(pos.get());
+					groups.get(i).dirty();
+				}
+			}
 		}
 		
+		buf.fill(255);
 		buf.stroke(0);
 		buf.strokeWeight(2);
 		
 		buf.ellipse(pos.x, pos.y, 2*radius, 2*radius);
+		
+		for (int i = interior.size()-1; i >= 0; i--) {
+			interior.get(i).display(buf);
+		}
+		for (int i = inputs.size()-1; i >= 0; i--) {
+			inputs.get(i).display(buf);
+		}
+		for (int i = groups.size()-1; i >= 0; i--) {
+			groups.get(i).display(buf);
+		}
+		output.display(buf);
+	}
+	
+	public void dirty() {
+		isDirty = true;
 	}
 	
 	public void addInput(Input inInput) {
@@ -64,6 +93,14 @@ public class Group {
 		return radius;
 	}
 	
+	public PVector getPos() {
+		return pos.get();
+	}
+	
+	public void setPos(PVector pos) {
+		this.pos = pos.get();
+	}
+	
 	public Group getParent() {
 		return parent;
 	}
@@ -74,7 +111,13 @@ public class Group {
 				return inputs.get(i);
 			}
 		}
-		return null;
+		
+		/* Search upwards */
+		if (parent == null) {
+			return null;
+		} else {
+			return parent.getInput(arg);
+		}
 	}
 	
 	public Output getOutput() {
@@ -86,14 +129,34 @@ public class Group {
 	private float calculateBestRadius() {
 		float defaultRadius = 10.0f;
 		
+		float totalInputDiams = (inputs.size() == 1) ? 15.0f : 5.0f;
+		for (int i = inputs.size()-1; i >= 0; i--) {
+			totalInputDiams += (2*inputs.get(i).getRadius());
+		}
+		/* The total size of the inputs cannot be more than (PI/2) * radius */
+		float radiusByInputs = totalInputDiams/((float) Math.PI/2);
+		
+		float groupArea = 1000.0f;
+		for (int i = groups.size()-1; i >= 0; i--) {
+			groupArea += Math.pow(groups.get(i).getRadius(), 2);
+		}
+		float radiusbyGroups = (float) Math.sqrt(groupArea);
+		
+		return Math.max(defaultRadius, Math.max(radiusByInputs, radiusbyGroups));
+	}
+	
+	private void distributeInputs() {
 		float totalInputDiams = 0.0f;
 		for (int i = inputs.size()-1; i >= 0; i--) {
 			totalInputDiams += (2*inputs.get(i).getRadius());
 		}
-		/* The total size of the inputs cannot be more than PI * radius */
-		float radiusByInputs = totalInputDiams/((float) Math.PI);
 		
-		
-		return Math.max(defaultRadius, radiusByInputs);
+		float angularPos = 0.0f;
+		for (int i = inputs.size()-1; i >= 0; i--) {
+			angularPos += (inputs.get(i).getRadius()/totalInputDiams) * Math.PI;
+			inputs.get(i).setPos(new PVector(pos.x+((float)Math.cos(angularPos)*radius), 
+					pos.y+((float)-Math.sin(angularPos)*radius)));
+			angularPos += (inputs.get(i).getRadius()/totalInputDiams) * Math.PI;
+		}
 	}
 }
