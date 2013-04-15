132f9be931518fd88a37828bd89db9ddaba95da5
Sam Gruber <grubermensch@gmail.com>
2013-04-15 00:17:27 -0400

    generalized draw code

--- src/com/scgruber/graphlambda/Merge.java
+++ src/com/scgruber/graphlambda/Merge.java
@@ -12,7 +12,7 @@ public class Merge extends Node {
 		this.pos = new PVector(0,0);
 		this.in = new ArrayList();
 		in.add(fn);
-		in.add(arg);
+		if (arg != null) in.add(arg);
 		this.out = new ArrayList();
 		out.add(res);
 		this.movable = true;
@@ -22,20 +22,16 @@ public class Merge extends Node {
 		buf.fill(255);
 		buf.stroke(0);
 		
-		/* Invariant: in.size() == 2; out.size() == 1 */
-		PVector toFn = PVector.sub(in.get(0).getPos(), pos);
-		PVector toArg = PVector.sub(in.get(1).getPos(), pos);
-		PVector toRes = PVector.sub(out.get(0).getPos(), pos);
-
-		buf.line(pos.x, pos.y, pos.x+toFn.x, pos.y+toFn.y);
-		buf.line(pos.x, pos.y, pos.x+toArg.x, pos.y+toArg.y);
-		buf.line(pos.x, pos.y, pos.x+toRes.x, pos.y+toRes.y);
+		/* Reposition to dead center between in and out */
+		pos = PVector.lerp(in.get(0).getPos(), out.get(0).getPos(), 0.5f);
 		
-		toFn.normalize();
-		toArg.normalize();
-		toRes.normalize();
-		buf.triangle((5*toFn.x) + pos.x, (5*toFn.y) + pos.y,
-				(5*toArg.x) + pos.x, (5*toArg.y) + pos.y,
-				(5*toRes.x) + pos.x, (5*toRes.y) + pos.y);  
+		for (int i = in.size()-1; i >= 0; i--) {
+			PVector to = in.get(i).getPos();
+			buf.line(pos.x, pos.y, to.x, to.y);
+		}
+		for (int i = out.size()-1; i >= 0; i--) {
+			PVector to = out.get(i).getPos();
+			buf.line(pos.x, pos.y, to.x, to.y);
+		} 
 	}
 }
